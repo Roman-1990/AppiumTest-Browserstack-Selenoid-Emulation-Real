2e078808bcbcd3f199f3548c761c4e37b2056a8e
Roman <pcuxman@yandex.ru>
2021-08-25 21:37:35 +0300

    Add tests selenide

--- src/test/java/tests/selenoid/SelenoidWikiTests.java
+++ src/test/java/tests/selenoid/SelenoidWikiTests.java
@@ -7,20 +7,30 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class SelenoidWikiTests extends SelenoidTestBase {
 
     @Test
     @Tag("selenoid")
     void searchTest() {
-        open("https://www.wikipedia.org/");
-        $(byXpath("//*[@id='searchLanguage']")).click();
-        $(byXpath("//*[@id='searchLanguage']/option[48]")).click();
-        $(byXpath("//*[@id='jsLangLabel']")).shouldHave(text("ru"));
-        $("#searchInput").val("java").click();
-        $$("#typeahead-suggestions").findBy(visible)
-                .shouldHave(text("язык программирования"));
-        $$(".suggestion-link").first().click();
-        $(".toctext").shouldHave(text("История создания"));
+        step("Open wiki", () -> {
+            open("https://www.wikipedia.org/");
+        });
+        step("Options customization", () -> {
+            $(byXpath("//*[@id='searchLanguage']")).click();
+            $(byXpath("//*[@id='searchLanguage']/option[48]")).click();
+            $(byXpath("//*[@id='jsLangLabel']")).shouldHave(text("ru"));
+        });
+        step("enter java", () -> {
+            $("#searchInput").val("java").click();
+            $$("#typeahead-suggestions").findBy(visible)
+                    .shouldHave(text("язык программирования"));
+
+            $$(".suggestion-link").first().click();
+        });
+        step("checking the text", () -> {
+            $(".toctext").shouldHave(text("История создания"));
+        });
     }
 }
