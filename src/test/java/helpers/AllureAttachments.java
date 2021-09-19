98b8176d6648c42104dbcc03a11771cafc05808a
Roman <pcuxman@yandex.ru>
2021-09-19 14:44:31 +0300

    Update AllureAttachments

--- src/test/java/helpers/AllureAttachments.java
+++ src/test/java/helpers/AllureAttachments.java
@@ -5,11 +5,9 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.localStorage;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 
